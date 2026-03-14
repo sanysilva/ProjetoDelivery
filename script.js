cff3f0bc0efa420f88da922d26085569d423506d
Sany Silva <sanypereirasilva@gmail.com>
2026-03-14 00:01:14 -0300

    CSS Atualizado

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirModal(){
+  document.getElementById("modal").style.display="block";
+}
+
+function fecharModal(){
+  document.getElementById("modal").style.display="none";
+}
+
+function alert("sou gay")
